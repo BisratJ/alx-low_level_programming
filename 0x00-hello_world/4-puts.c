ea706f7de3aed44fe406373c8008121c9e7b799f
Bisrat Gizaw <bisrojc60@gmail.com>
2022-06-03 06:09:31 -0700

    task2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
-
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
+/*  * main - Entry point
  *    * Return: Always 0 (Success)
  *
  *     */
@@ -16,8 +9,11 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	 puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	  return (0);
 
 }
+
+
+
