8287b20c06920d13353db02ec025f083a22b567f
Bisrat Gizaw <bisrojc60@gmail.com>
2022-06-04 04:03:00 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - main block
  * Description: Get a random number and print the number
- * and if it is positive, negative, or zero 
+ * and if it is positive, negative, or zero
  * Return: 0
  */
 int main(void)
