816930675e5b19ffc812cea8f233be09a9fb561a
Bisrat Gizaw <bisrojc60@gmail.com>
2022-06-04 08:02:39 -0700

    correction of white space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,8 +4,8 @@
  * main - main block
  * Description: Write a program that prints all possible
  * different combination of two digits
- * Numbers must be separated by ,, followed by a space 
- * The two digits must be different 
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
  * 01 and 10 are considered the same combination of the top digits 0 and 1
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
