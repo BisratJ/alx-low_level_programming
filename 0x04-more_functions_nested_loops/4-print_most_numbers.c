61c0b838417baca18fbdd8f3a5be2d2b994088fd
Bisrat7 <bisrojc60@gmail.com>
2022-06-10 15:25:03 +0300

    prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9.
  * Do not print 2 and 4.
@@ -7,12 +8,12 @@
  */
 void print_most_numbers(void)
 {
-   int i = '0';
-   for(; i <= '9'; i++)
-     {
-       if(i != '2' && i != '4')
-         
-           _putchar(i);                    
-     } 
-       _puthcar('\n'); 
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
 }
