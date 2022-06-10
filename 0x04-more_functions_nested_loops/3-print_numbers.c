8f84fb2bac41f8e1fea2f00fd39a402620238ac3
Bisrat7 <bisrojc60@gmail.com>
2022-06-10 14:51:03 +0300

    Prints form 0=9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *num - integer.
+ *Return: void.
+ */
+
+void print_numbers(void){
+   int num = 0;
+   while(num < 10)
+   {
+     printf("%d\n", num);
+     num ++;
+   }
+  return(0);
+}
