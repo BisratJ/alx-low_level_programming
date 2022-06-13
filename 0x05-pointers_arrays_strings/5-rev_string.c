e57eda0a079353daad3a168ea952df469660ee37
Bisrat7 <bisrojc60@gmail.com>
2022-06-13 22:49:32 +0300

    Revers a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * puts2 - print alternating chars of string
- * @str: string
+ * rev_string - reverse string
+ * @s: string
  * Return: void
  */
 
-void puts2(char *str)
+void rev_string(char *s)
 {
-	int i;
+	int i, max, half;
+	char first, last;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
 }
