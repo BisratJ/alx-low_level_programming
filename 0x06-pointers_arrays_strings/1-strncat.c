713f6122898a740e5b121689e380dbc715fd5e1a
Bisrat7 <bisrojc60@gmail.com>
2022-06-15 18:29:10 +0300

    concatinates 2 strings using atmost bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: n bytes 
+ * Return: the pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n){
+  int count = 0, count2 = 0;
+while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
