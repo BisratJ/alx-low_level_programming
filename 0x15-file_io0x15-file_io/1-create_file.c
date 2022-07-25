885a6f49561716b2325c1cf17c522ece3e3ccf1d
Bisrat7 <bisrojc60@gmail.com>
2022-07-26 00:14:38 +0300

    commit-1

--- 0x15-file_io0x15-file_io/1-create_file.c
+++ 0x15-file_io0x15-file_io/1-create_file.c
@@ -1,36 +1,38 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
+ * create_file - creates a file
  * @filename: filename.
- * @letters: numbers of letters printed.
+ * @text_content: content writed in the file.
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: 1 if it success. -1 if it fails.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int nletters;
+	int rwr;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
+	if (!text_content)
+		text_content = "";
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
 
-	free(buf);
-
-	return (nwr);
+	return (1);
 }
