77248457d084208d0cf9feedbab9cf16c8f9df81
Bisrat Gizaw <bisrojc60@gmail.com>
2024-04-04 11:18:22 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,26 @@
-#include "main.h"
+// Online C compiler to run C program online
+#include <stdio.h>
 
-/**
- * _isalpha - checks for alphabetic character
- * @c: c is an ascii character
- *
- * Return: 1 (if letter)
- */
+int _islower(int c){
+    
+    if(c >= 'a' && c <= 'z'){
+      return(1);  
+    }
+    
+    else if(c >= 'A' && c <= 'Z'){
+        return(1);
+    }
+    
+    else{
+        return(0);
+    }
+}
 
-int _isalpha(int c)
-{
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+int main() {
+    // Write C code here
+    char ltr = '@';
+    int is_lower = _islower((int)ltr);
+    printf("%d", is_lower);
+    return 0;
 }
